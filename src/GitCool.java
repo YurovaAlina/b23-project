356db741eb84f23c0c8b734e4cfed037bb711f5c
YurovaAlina <kulunbiegova@gmail.com>
2021-07-19 15:42:18 -0500

    added github account

--- src/GitCool.java
+++ src/GitCool.java
@@ -2,5 +2,7 @@ public class GitCool {
     //B23 is learning git
     // B23 is a very special number
     //B23 is making some sense now
+    //We have added out account under Intellij
+    //make sure to click apply
 
 }
